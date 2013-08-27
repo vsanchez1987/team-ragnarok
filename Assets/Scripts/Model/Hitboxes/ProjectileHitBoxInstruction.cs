89bc3248e0b9f42f6141d7096ffc216d132cf231
hnguyen091188 <hnguyen091188@yahoo.com>
2013-08-27 16:35:50 -0700

    fix Projectile_seek

--- Assets/Scripts/Model/Hitboxes/ProjectileHitBoxInstruction.cs
+++ Assets/Scripts/Model/Hitboxes/ProjectileHitBoxInstruction.cs
@@ -10,6 +10,7 @@ namespace FightGame{
 		private Transform	startJoint;
 		private Vector3		increment;
 		private Vector3		offset;
+		private float		speed;
 		/*
 		public ProjectileHitBoxInstruction( string projectileName, string startJoint, Vector3 direction, float speed, A_Fighter fighter, float radius, float damage, float startTime, float endTime, Vector3 offset = default(Vector3), Vector3 movement = default(Vector3)) : base(fighter, radius, damage, startTime, endTime, movement){
 			float timePeriod	= (endTime - startTime);
@@ -25,6 +26,7 @@ namespace FightGame{
 			this.startJoint		= fighter.joints[startJoint];
 			this.increment		= direction.normalized * speed * (Time.deltaTime);
 			this.offset			= offset;
+			this.speed			= speed;
 		}
 		
 		public override void Start(){
@@ -34,6 +36,7 @@ namespace FightGame{
 			ProjectileInput pInput = projectile.GetComponent<ProjectileInput>();
 			pInput.direction = new Vector3(this.increment.x * fighter.GlobalForwardVector.x, this.increment.y, this.increment.z);
 			pInput.hitbox = new HitBox( this.fighter, pInput.hitboxObject, true );
+			pInput.speed = this.speed;
 			this.hitbox = pInput.hitbox;
 			
 			HitBoxInput hInput = pInput.hitboxObject.GetComponent<HitBoxInput>();
--- Assets/Scripts/Model/Hitboxes/ProjectileInput.cs
+++ Assets/Scripts/Model/Hitboxes/ProjectileInput.cs
@@ -9,28 +9,47 @@ public class ProjectileInput : MonoBehaviour
 	
 	[HideInInspector]
 	public Vector3		direction;
+	public Vector3		newDirection;
 	[HideInInspector]
 	public HitBox 		hitbox;
-	float timer = 0f;
+	float timer;
 	Vector3 target;
+	[HideInInspector]
+	public float speed;
 	
 	void Start()
 	{
-		target = hitbox.GetTarget().gobj.transform.position;
+		this.target = hitbox.GetTarget().hurtBoxes["HurtBox_Hip"].gobj.transform.position;
+		this.newDirection = Vector3.zero;
+		timer = 0f;
 	}
 	
 	void Update(){
-		timer +=Time.deltaTime;
-		this.transform.position += direction;
+		
 		//Debug.Log(this.name);
-		if(this.name == "Projectile_Seek(Clone)")
+		
+		if(timer > 0.7f)
 		{
-			if(timer > 0.5f)
+			if(this.name == "Projectile_Seek(Clone)")
 			{
-				Vector3 newdirection = target - this.transform.position;
-				this.transform.Translate(newdirection*5*Time.deltaTime);
+				if ( this.newDirection == Vector3.zero){
+					this.newDirection = target - this.transform.position;
+				}
+				this.transform.position += newDirection.normalized * speed * Time.deltaTime;
+				//this.transform.Translate(newDirection*5*Time.deltaTime);
+			}
+			else{
+				this.transform.position += direction;
 			}
 		}
+		else{
+			this.transform.position += direction;
+		}
+		if(this.transform.position.y < 0){
+			GameObject.Destroy(this.gameObject);
+		}
+		
+		timer +=Time.deltaTime;
 	}
 }
 
